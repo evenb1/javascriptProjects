3284269ede6d1b9881d0b8d88ef7409b5d91f221
Even <even.berhane2022@gmail.com>
2024-06-04 22:34:45 +0300

    card function

---
+++ card-component/src/Card.jsx
@@ -0,0 +1,11 @@
+
+function Card(){
+    return(
+        <div className="card">
+            <img></img>
+            <h2>Even</h2>
+            <p>IM aa student</p>
+        </div>
+    )
+}
+export default Card
